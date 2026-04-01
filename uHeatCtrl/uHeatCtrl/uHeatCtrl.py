b45298d4a49c1a73b4743465143f05fdd623e420
Robert Sheehan <robertnsheehan@gmail.com>
2026-04-01 15:07:44 +0100

    Offset Cal
    Does the offset of the offset reduce the offset? Analyse this data!

--- uHeatCtrl/uHeatCtrl/uHeatCtrl.py
+++ uHeatCtrl/uHeatCtrl/uHeatCtrl.py
@@ -49,7 +49,7 @@ if __name__ == '__main__':
         Loud = False
         MCVS_Lib.Long_Measurement(board_name, pinOuts, Time, Nmeas, Loud)
 
-    OFFSET_CALIB = False
+    OFFSET_CALIB = True
     if OFFSET_CALIB:
         #board_name = 'Through_Hole'
         board_name = 'Four_Channel_PCB'
@@ -59,13 +59,12 @@ if __name__ == '__main__':
         Loud = True
         MCVS_Lib.Offset_Calibration(board_name, pinOuts, Nmeas, crrctOffst, Loud)
 
-    OFFSET_CALIB_ANAL = True
+    OFFSET_CALIB_ANAL = False
     if OFFSET_CALIB_ANAL:
         #board_name = 'Through_Hole'
         board_name = 'Four_Channel_PCB'
         pinOuts = ["V1", "V2", "V3", "V4"]
         Nmeas = 1001 # total no. measurements
-        zero_outputs = True
         Loud = True
         MCVS_Lib.Offset_Calibration_Analysis(board_name, pinOuts, Nmeas, Loud)
 
